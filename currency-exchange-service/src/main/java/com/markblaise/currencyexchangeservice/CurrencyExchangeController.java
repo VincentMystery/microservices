0d636357f640a24a9d51e40c7696e6158a4d72bc
VincentMystery <markblaise@gmail.com>
2019-03-06 14:04:05 -0700

    refactor(git): reset to master

---
+++ currency-exchange-service/src/main/java/com/markblaise/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,26 @@
+package com.markblaise.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment enviroment;
+	
+	@Autowired
+	private ExchangeValueRepository exchangeValRepo;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		ExchangeValue exchangeValue = exchangeValRepo.findByFromAndTo(from, to);
+		exchangeValue.setPort(Integer.parseInt(enviroment.getProperty("local.server.port")));
+
+		return exchangeValue;
+	}
+}
